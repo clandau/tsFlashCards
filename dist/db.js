3695cb51259b28c9cb11c8169d323382bd62cd21
clandau <so.super@gmail.com>
2019-04-17 11:39:09 -0400

    slight changes

--- dist/db.js
+++ dist/db.js
@@ -3,16 +3,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// const mysql = require('promise-mysql');
+const dotenv_1 = __importDefault(require("dotenv"));
 const promise_mysql_1 = __importDefault(require("promise-mysql"));
-require('dotenv').config();
+dotenv_1.default.config();
 const config = {
+    connectionLimit: 100,
+    database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
+    password: process.env.DB_PASS,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_DATABASE,
-    connectionLimit: 100,
 };
 const pool = promise_mysql_1.default.createPool(config);
 exports.default = pool;
--- dist/server.js
+++ dist/server.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
@@ -17,9 +17,9 @@ const koa_static_1 = __importDefault(require("koa-static"));
 const routes = require("./routes");
 const app = new koa_1.default();
 const pug = new koa_pug_1.default({
-    viewPath: './views',
-    basedir: './views',
-    app: app
+    app,
+    basedir: "./views",
+    viewPath: "./views",
 });
 app.use((ctx, next) => __awaiter(this, void 0, void 0, function* () {
     try {
@@ -28,10 +28,9 @@ app.use((ctx, next) => __awaiter(this, void 0, void 0, function* () {
     catch (err) {
         ctx.status = 400;
         ctx.body = `oh noes! ${err.message}`;
-        console.log(`error handler: ${err.message}`);
     }
 }));
-app.use(koa_static_1.default('.'));
+app.use(koa_static_1.default("."));
 app.use(routes);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
