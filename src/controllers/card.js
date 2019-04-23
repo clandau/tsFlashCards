1fddf1e8aeb3813bfb59ada34f461f545ae503d2
clandau <so.super@gmail.com>
2019-04-23 10:02:11 -0400

    move business logic out of routing

---
+++ src/controllers/card.js
@@ -0,0 +1,45 @@
+class CardController {
+    constructor() {}
+
+    async all(ctx) {
+        const sql = "SELECT * FROM card ORDER BY category";
+        try {
+            const data = await pool.query(sql);
+            ctx.status = 200;
+            ctx.body = data;
+        } catch (err) {
+            ctx.throw(400, `${err}`);
+        }
+    }
+
+    async random(ctx) {
+        let sql;
+        let category = ctx.params.category;
+
+        if (!category) {
+            sql = "SELECT category, sideA, sideB FROM card ORDER BY RAND() LIMIT 1";
+        } else {
+            sql = "SELECT category, sideA, sideB FROM card WHERE category=? ORDER BY RAND() LIMIT 1";
+        }
+        try {
+            const data = await pool.query(sql);
+            category = data[0].category;
+            ctx.render("card", { cardData : data, category });
+        } catch (err) {
+            ctx.throw(400, `${err}`);
+        }
+    }
+
+    async newCard(ctx) {
+        const request = ctx.request.body;
+        try {
+            const sql = `INSERT INTO card set ?`;
+            await pool.query(sql, [request]);
+            ctx.render("index", { message: "Sucessfully added new card to database." });
+        } catch (err) {
+            ctx.throw(400, `post error: ${err}`);
+        }
+    }
+}
+
+export default new CardController();
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import koaBody from "koa-body";
 import Router from "koa-router";
 
+import CardController from "./controllers/card";
 import pool from "./db";
 
 const router = new Router();
@@ -11,42 +12,15 @@ router.get("/", async (ctx) => {
 });
 
 router.get("/all", async (ctx) => {
-    const sql = "SELECT * FROM card ORDER BY category";
-    try {
-        const data = await pool.query(sql);
-        ctx.status = 200;
-        ctx.body = data;
-    } catch (err) {
-        ctx.throw(400, `${err}`);
-    }
+    await CardController.all(ctx);
 });
 
 router.get("/random", async (ctx) => {
-    let category = ctx.params.category;
-    let sql;
-    if (!category) {
-        sql = "SELECT category, sideA, sideB FROM card ORDER BY RAND() LIMIT 1";
-    } else {
-        sql = "SELECT category, sideA, sideB FROM card WHERE category=? ORDER BY RAND() LIMIT 1";
-    }
-    try {
-        const data = await pool.query(sql);
-        category = data[0].category;
-        ctx.render("card", { cardData : data, category });
-    } catch (err) {
-        ctx.throw(400, `${err}`);
-    }
+    await CardController.random(ctx);
 });
 
 router.post("/new", koaBody(), async (ctx) => {
-    const data = ctx.request.body;
-    try {
-        const sql = `INSERT INTO card set ?`;
-        await pool.query(sql, [data]);
-        ctx.render("index", { message : "Sucessfully added new card to database."});
-    } catch (err) {
-        ctx.throw(400, `post error: ${err}`);
-    }
+    await CardController.newCard(ctx);
 });
 
 export = router.routes();
